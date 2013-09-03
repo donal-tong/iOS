56376be6ab46f70f820c786699933d36c28343d1
Donal <donal.tong@gmail.com>
2013-09-03 21:36:35 +0800

    add veno
    short video project

---
+++ veno/veno/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  veno
+//
+//  Created by Donal on 13-9-2.
+//  Copyright (c) 2013年 vikaa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
